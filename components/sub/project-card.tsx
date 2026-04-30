58bd764a488e1154e71299bf87ee22ad5d0bd7d0
Imran Patel <imrankpatel7861@gmail.com>
2026-04-30 16:15:43 +0530

    Project-card.tsx

--- components/sub/project-card.tsx
+++ components/sub/project-card.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import InvennzyAnimation from "./InvennzyAnimation";
+import TimelazAnimation from "./TimelazAnimation";
+import GradyzeAnimation from "./GradyzeAnimation";
 
 type ProjectCardProps = {
   src: string;
@@ -21,13 +24,27 @@ export const ProjectCard = ({
       rel="noreferrer noopener"
       className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]"
     >
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
+      {title.includes("Invennzy") ? (
+        <div className="w-full">
+          <InvennzyAnimation />
+        </div>
+      ) : title.includes("Timelazy") ? (
+        <div className="w-full">
+          <TimelazAnimation />
+        </div>
+      ) : title.includes("Gradyze") ? (
+        <div className="w-full">
+          <GradyzeAnimation />
+        </div>
+      ) : (
+        <Image
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
+      )}
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
